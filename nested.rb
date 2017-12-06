7d1998a54b1b818fcda8b0d2c57b3a597c8bc875
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-06 20:29:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -102,7 +102,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+		 programmer_hash[:alan_kay][:known_for]
+ 
+puts my_hash
 end
 
 def adding_to_dennis
